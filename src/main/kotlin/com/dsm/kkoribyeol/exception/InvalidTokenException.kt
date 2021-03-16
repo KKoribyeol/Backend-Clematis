5a175059a9768124e89bc0ed90fac448d0099246
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-16 16:28:05 +0900

    Remove InvalidTokenException.kt file.

--- src/main/kotlin/com/dsm/kkoribyeol/exception/InvalidTokenException.kt
+++
@@ -1,8 +0,0 @@
-package com.dsm.kkoribyeol.exception
-
-import org.springframework.security.core.AuthenticationException
-
-class InvalidTokenException(
-    val code: String = "INVALID_TOKEN",
-    message: String = "잘못된 토큰입니다.",
-) : AuthenticationException(message)
