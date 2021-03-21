b5e8aa09bd9d3f075edf1ce8ab6e612a2e6219aa
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-21 18:26:41 +0900

    add new function that modify target-group.

--- src/main/kotlin/com/dsm/kkoribyeol/controller/GroupController.kt
+++ src/main/kotlin/com/dsm/kkoribyeol/controller/GroupController.kt
@@ -2,10 +2,13 @@ package com.dsm.kkoribyeol.controller
 
 import com.dsm.kkoribyeol.controller.request.GroupRequest
 import com.dsm.kkoribyeol.service.GroupCreationService
+import com.dsm.kkoribyeol.service.GroupModificationService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 @RestController
@@ -13,19 +16,51 @@ import javax.validation.constraints.Size
 @Validated
 class GroupController(
     private val groupCreationService: GroupCreationService,
+    private val groupModificationService: GroupModificationService,
 ) {
 
     @PostMapping
     fun createGroup(
-        @Size(min = 9, max = 28, message = "<9~28>")
+        @Pattern(
+            regexp = "[a-zA-Z0-9]{1,20}-[a-zA-Z0-9]{7}",
+            message = "정규표현식: [a-zA-Z0-9]{1,20}-[a-zA-Z0-9]{7}"
+        )
+
+        @NotBlank(message = "<NULL> <EMPTY> <BLANK>")
         @RequestHeader("projectCode")
-        projectCode: String,
+        projectCode: String?,
+
         @Valid
         @NotNull(message = "Request Body 는 null 이 될 수 없습니다.")
         @RequestBody
         request: GroupRequest?,
     ) = groupCreationService.createGroup(
-        projectCode = projectCode,
+        projectCode = projectCode!!,
         groupName = request!!.groupName!!,
+    )
+
+    @PatchMapping("/{groupName}")
+    fun modifyGroup(
+        @Pattern(
+            regexp = "[a-zA-Z0-9]{1,20}-[a-zA-Z0-9]{7}",
+            message = "정규표현식: [a-zA-Z0-9]{1,20}-[a-zA-Z0-9]{7}"
+        )
+        @NotBlank(message = "<NULL> <EMPTY> <BLANK>")
+        @RequestHeader("projectCode")
+        projectCode: String?,
+
+        @Size(min = 1, max = 20, message = "<1~20>")
+        @NotBlank(message = "<NULL> <EMPTY> <BLANK>")
+        @PathVariable("groupName")
+        groupName: String?,
+
+        @Valid
+        @NotNull(message = "Request Body 는 null 이 될 수 없습니다.")
+        @RequestBody
+        request: GroupRequest?,
+    ) = groupModificationService.modifyGroupName(
+        projectCode = projectCode!!,
+        groupName = groupName!!,
+        newGroupName = request!!.groupName!!
     )
 }
--- src/main/kotlin/com/dsm/kkoribyeol/controller/request/GroupRequest.kt
+++ src/main/kotlin/com/dsm/kkoribyeol/controller/request/GroupRequest.kt
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 data class GroupRequest(
+
     @JsonProperty("name")
     @get:Size(min = 1, max = 20, message = "<1~20>")
     @get:NotBlank(message = "<NULL> <EMPTY> <BLANK>")
