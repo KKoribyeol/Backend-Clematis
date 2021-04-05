4fd585a3cec79f97c0e9f56da6a99ad8163af291
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 23:35:39 +0900

    modify test code because modified feature that template.

--- src/main/kotlin/com/dsm/clematis/domain/template/repository/TemplateRepository.kt
+++ src/main/kotlin/com/dsm/clematis/domain/template/repository/TemplateRepository.kt
@@ -4,5 +4,11 @@ import com.dsm.clematis.domain.template.domain.Template
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TemplateRepository : JpaRepository<Template, Long> {
-    fun existsByTitleAndBody(title: String, body: String): Boolean
+    fun findByProjectCode(projectCode: String): List<Template>
+    fun findByIdAndProjectCode(id: Long, projectCode: String): Template?
+
+    fun existsByIdAndProjectCode(id: Long, projectCode: String): Boolean
+    fun existsByTitleAndBodyAndProjectCode(title: String, body: String, projectCode: String): Boolean
+
+    fun deleteByIdAndProjectCode(id: Long, projectCode: String)
 }
