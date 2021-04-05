d2c1c52882024422878ff44b7bb7089ccf37d9ee
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 23:30:00 +0900

    modify test code because modified feature that template.

--- src/test/kotlin/com/dsm/clematis/domain/template/service/TemplateSearchServiceTest.kt
+++ src/test/kotlin/com/dsm/clematis/domain/template/service/TemplateSearchServiceTest.kt
@@ -1,5 +1,7 @@
 package com.dsm.clematis.domain.template.service
 
+import com.dsm.clematis.domain.account.domain.Account
+import com.dsm.clematis.domain.project.domain.Project
 import com.dsm.clematis.domain.template.domain.Template
 import com.dsm.clematis.domain.template.exception.TemplateNotFoundException
 import com.dsm.clematis.domain.template.repository.TemplateRepository
@@ -17,7 +19,9 @@ internal class TemplateSearchServiceTest {
     private val templateRepository = mockk<TemplateRepository>()
     private val testService = TemplateSearchService(templateRepository)
 
-    private val savedTemplate = Template("saved title", "saved body")
+    private val savedAccount = Account("savedIdId", "savedPassword", "savedName")
+    private val savedProject = Project("savedProject-finally", "savedProject", "savedDescription", savedAccount)
+    private val savedTemplate = Template("saved title", "saved body", savedProject)
 
     @BeforeEach
     fun setup() {
@@ -26,40 +30,40 @@ internal class TemplateSearchServiceTest {
 
     @Test
     fun `푸시 템플릿 단일 조회하기`() {
-        every { templateRepository.findByIdOrNull(1) } returns savedTemplate
-        every { templateRepository.findByIdOrNull(anyLong()) } returns null
+        every { templateRepository.findByIdAndProjectCode(1, savedProject.code) } returns savedTemplate
+        every { templateRepository.findByIdAndProjectCode(anyLong(), any()) } returns null
 
-        val findTemplate = testService.searchTemplate(1)
+        val findTemplate = testService.searchTemplate(1, "savedProject-finally")
 
         assertThat(findTemplate.id).isEqualTo(savedTemplate.id)
         assertThat(findTemplate.title).isEqualTo(savedTemplate.title)
         assertThat(findTemplate.body).isEqualTo(savedTemplate.body)
 
-        verify(exactly = 1) { templateRepository.findByIdOrNull(1) }
-        verify(exactly = 0) { templateRepository.findAll() }
+        verify(exactly = 1) { templateRepository.findByIdAndProjectCode(1, savedProject.code) }
+        verify(exactly = 0) { templateRepository.findByProjectCode(any()) }
     }
 
     @Test
     fun `푸시 템플릿 단일 조회하기 - throw TemplateNotFoundException`() {
-        every { templateRepository.findByIdOrNull(1) } returns savedTemplate
-        every { templateRepository.findByIdOrNull(2) } returns null
+        every { templateRepository.findByIdAndProjectCode(1, savedProject.code) } returns savedTemplate
+        every { templateRepository.findByIdAndProjectCode(anyLong(), any()) } returns null
 
-        assertThrows<TemplateNotFoundException> { testService.searchTemplate(2) }
+        assertThrows<TemplateNotFoundException> { testService.searchTemplate(2, "savedProject-finally") }
 
-        verify(exactly = 1) { templateRepository.findByIdOrNull(2) }
-        verify(exactly = 0) { templateRepository.findAll() }
+        verify(exactly = 1) { templateRepository.findByIdAndProjectCode(2, savedProject.code) }
+        verify(exactly = 0) { templateRepository.findByProjectCode(any()) }
     }
 
     @Test
     fun `푸시 템플릿 전체 조회하기`() {
-        every { templateRepository.findAll() } returns listOf(savedTemplate)
+        every { templateRepository.findByProjectCode(savedProject.code) } returns listOf(savedTemplate)
 
-        val findTemplates = testService.searchAllTemplate()
+        val findTemplates = testService.searchAllTemplate("savedProject-finally")
 
         assertThat(findTemplates).map<String>{ it.title }.containsAll(listOf(savedTemplate.title))
         assertThat(findTemplates).map<String>{ it.body }.containsAll(listOf(savedTemplate.body))
 
-        verify(exactly = 0) { templateRepository.findByIdOrNull(anyLong()) }
-        verify(exactly = 1) { templateRepository.findAll() }
+        verify(exactly = 0) { templateRepository.findByIdAndProjectCode(anyLong(), any()) }
+        verify(exactly = 1) { templateRepository.findByProjectCode(savedProject.code) }
     }
 }
