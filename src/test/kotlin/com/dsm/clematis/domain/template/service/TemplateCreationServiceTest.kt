58a9cd9df60a93f6d93d8c873bb6eb970c9e6e9a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 23:32:44 +0900

    modify test code because modified feature that template.

--- src/test/kotlin/com/dsm/clematis/domain/template/service/TemplateCreationServiceTest.kt
+++ src/test/kotlin/com/dsm/clematis/domain/template/service/TemplateCreationServiceTest.kt
@@ -1,5 +1,8 @@
 package com.dsm.clematis.domain.template.service
 
+import com.dsm.clematis.domain.account.domain.Account
+import com.dsm.clematis.domain.project.domain.Project
+import com.dsm.clematis.domain.project.repository.ProjectRepository
 import com.dsm.clematis.domain.template.domain.Template
 import com.dsm.clematis.domain.template.exception.AlreadyExistTemplateException
 import com.dsm.clematis.domain.template.repository.TemplateRepository
@@ -10,13 +13,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.springframework.data.repository.findByIdOrNull
 
 internal class TemplateCreationServiceTest {
     private val templateRepository = mockk<TemplateRepository>()
-    private val testService = TemplateCreationService(templateRepository)
+    private val projectRepository = mockk<ProjectRepository>()
+    private val testService = TemplateCreationService(templateRepository, projectRepository)
 
-    private val savedTemplate = Template("saved title", "saved body")
-    private val newTemplate = Template("title", "body")
+    private val savedAccount = Account("savedIdId", "savedPassword", "savedName")
+    private val savedProject = Project("savedProject-finally", "savedProject", "savedDescription", savedAccount)
+    private val savedTemplate = Template("saved title", "saved body", savedProject)
+    private val newTemplate = Template("title", "body", savedProject)
 
     @BeforeEach
     fun setup() {
@@ -26,26 +33,28 @@ internal class TemplateCreationServiceTest {
 
     @Test
     fun `푸시 템플릿 생성하기`() {
-        every { templateRepository.existsByTitleAndBody(savedTemplate.title, savedTemplate.body) } returns true
-        every { templateRepository.existsByTitleAndBody(newTemplate.title, newTemplate.body) } returns false
+        every { templateRepository.existsByTitleAndBodyAndProjectCode(savedTemplate.title, savedTemplate.body, "savedProject-finally") } returns true
+        every { templateRepository.existsByTitleAndBodyAndProjectCode(newTemplate.title, newTemplate.body, "savedProject-finally") } returns false
         every { templateRepository.save(any()) } returns newTemplate
+        every { projectRepository.findByIdOrNull(savedProject.code) } returns savedProject
 
-        val creationNumber = testService.createTemplate("title", "body")
+        val creationNumber = testService.createTemplate("title", "body", "savedProject-finally")
 
         assertThat(creationNumber).isEqualTo(2)
-        verify(exactly = 1) { templateRepository.existsByTitleAndBody(newTemplate.title, newTemplate.body) }
+        verify(exactly = 1) { templateRepository.existsByTitleAndBodyAndProjectCode(newTemplate.title, newTemplate.body, "savedProject-finally") }
         verify(exactly = 1) { templateRepository.save(any()) }
     }
 
     @Test
     fun `푸시 템플릿 생성하기 - throw AlreadyExistTemplateException`() {
-        every { templateRepository.existsByTitleAndBody(savedTemplate.title, savedTemplate.body) } returns true
-        every { templateRepository.existsByTitleAndBody(newTemplate.title, newTemplate.body) } returns false
+        every { templateRepository.existsByTitleAndBodyAndProjectCode(savedTemplate.title, savedTemplate.body, "savedProject-finally") } returns true
+        every { templateRepository.existsByTitleAndBodyAndProjectCode(newTemplate.title, newTemplate.body, "savedProject-finally") } returns false
         every { templateRepository.save(any()) } returns newTemplate
+        every { projectRepository.findByIdOrNull(savedProject.code) } returns savedProject
 
-        assertThrows<AlreadyExistTemplateException> { testService.createTemplate("saved title", "saved body") }
+        assertThrows<AlreadyExistTemplateException> { testService.createTemplate("saved title", "saved body", "savedProject-finally") }
 
-        verify(exactly = 1) { templateRepository.existsByTitleAndBody(savedTemplate.title, savedTemplate.body) }
+        verify(exactly = 1) { templateRepository.existsByTitleAndBodyAndProjectCode(savedTemplate.title, savedTemplate.body, "savedProject-finally") }
         verify(exactly = 0) { templateRepository.save(any()) }
     }
 }
