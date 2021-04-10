1898e25ed76fd1b30ba3cea19fb874c5dbc71eaf
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-10 23:15:36 +0900

    Add dependency that spring data redis.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("mysql:mysql-connector-java")
 
