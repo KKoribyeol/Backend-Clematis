933ae208b44d6d1e83ad5433ed7ad2889b550308
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-10 23:14:26 +0900

    Add redis repository.

--- src/main/kotlin/com/dsm/clematis/domain/account/repository/RedisRepository.kt
+++ src/main/kotlin/com/dsm/clematis/domain/account/repository/RedisRepository.kt
@@ -1,4 +1,6 @@
 package com.dsm.clematis.domain.account.repository
 
-interface RedisRepository {
-}
+import com.dsm.clematis.domain.account.domain.RefreshToken
+import org.springframework.data.repository.CrudRepository
+
+interface RedisRepository : CrudRepository<RefreshToken, String>
