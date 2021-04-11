b5fa0f4c865ed2c1f6d2f6aa4ac82428960b31e8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:47:02 +0900

    add test code that authentication delete.

--- src/test/kotlin/com/dsm/clematis/domain/authentication/service/AuthenticationDeletionServiceTest.kt
+++ src/test/kotlin/com/dsm/clematis/domain/authentication/service/AuthenticationDeletionServiceTest.kt
@@ -1,5 +1,31 @@
 package com.dsm.clematis.domain.authentication.service
 
-import org.junit.jupiter.api.Assertions.*
+import com.dsm.clematis.domain.account.domain.Account
+import com.dsm.clematis.domain.authentication.domain.RefreshToken
+import com.dsm.clematis.domain.authentication.repository.RedisRepository
+import io.mockk.*
+import org.junit.jupiter.api.Test
 
-internal class AuthenticationDeletionServiceTest
+internal class AuthenticationDeletionServiceTest {
+    private val redisRepository = mockk<RedisRepository>()
+    private val testService = AuthenticationDeletionService(
+        redisRepository = redisRepository,
+    )
+
+    private val savedAccount = Account(
+        id = "savedIdId",
+        password = "savedPassword",
+        name = "savedName",
+    )
+
+    @Test
+    fun `레디스 토큰 삭제`() {
+        every { redisRepository.deleteById(savedAccount.id) } just Runs
+
+        testService.deleteToken(
+            accountId = "savedIdId",
+        )
+
+        verify(exactly = 1) { redisRepository.deleteById(savedAccount.id) }
+    }
+}
