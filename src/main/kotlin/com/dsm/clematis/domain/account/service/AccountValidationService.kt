4850cdcc0169744e2cb6bd12408027a35a86b8f1
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 22:16:54 +0900

    modify account validate.

--- src/main/kotlin/com/dsm/clematis/domain/account/service/AccountValidationService.kt
+++ src/main/kotlin/com/dsm/clematis/domain/account/service/AccountValidationService.kt
@@ -1,4 +1,27 @@
 package com.dsm.clematis.domain.account.service
 
-class AccountValidationService {
+import com.dsm.clematis.domain.account.exception.AccountNotFoundException
+import com.dsm.clematis.domain.account.repository.AccountRepository
+import com.dsm.clematis.domain.account.exception.PasswordMismatchException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class AccountValidationService(
+    private val passwordEncoder: PasswordEncoder,
+    private val accountRepository: AccountRepository,
+) {
+
+    fun validateAccount(accountId: String, accountPassword: String): Boolean {
+        val findAccount = findAccountById(accountId)
+
+        return if (passwordEncoder.matches(accountPassword, findAccount.password))
+            true
+        else
+            throw PasswordMismatchException(accountPassword)
+    }
+
+    private fun findAccountById(accountId: String) =
+        accountRepository.findByIdOrNull(accountId) ?: throw AccountNotFoundException(accountId)
 }
