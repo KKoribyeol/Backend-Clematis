8561f0c10f69a6c58d151b248dab0eb5c90d32e7
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:57:45 +0900

    add exception that token not found.

--- src/main/kotlin/com/dsm/clematis/domain/authentication/exception/TokenNotFoundException.kt
+++ src/main/kotlin/com/dsm/clematis/domain/authentication/exception/TokenNotFoundException.kt
@@ -1,4 +1,12 @@
 package com.dsm.clematis.domain.authentication.exception
 
-class TokenNotFoundException {
-}
+import com.dsm.clematis.global.exception.CommonException
+import org.springframework.http.HttpStatus
+
+class TokenNotFoundException(
+    accountId: String,
+) : CommonException(
+    code = "TOKEN_NOT_FOUND",
+    message = "레디스에서 리프레시 토큰을 찾을 수 없습니다. [$accountId]",
+    status = HttpStatus.NOT_FOUND,
+)
