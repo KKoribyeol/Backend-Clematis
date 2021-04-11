ed0d9da28fec54c9cfc12045cf7106ee3f50f6c3
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:48:18 +0900

    add test code that redis repositry.

--- src/test/kotlin/com/dsm/clematis/domain/authentication/repository/TestRedisRepository.kt
+++ src/test/kotlin/com/dsm/clematis/domain/authentication/repository/TestRedisRepository.kt
@@ -1,4 +1,38 @@
 package com.dsm.clematis.domain.authentication.repository
 
-class TestRedisRepository {
+import com.dsm.clematis.domain.authentication.domain.RefreshToken
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Primary
+@Repository
+class TestRedisRepository : RedisRepository {
+    private val redis = mutableMapOf(
+        "savedIdId" to RefreshToken(
+            accountId = "savedIdId",
+            refreshToken = "this-is-test-token",
+        )
+    )
+
+    override fun <S : RefreshToken> save(entity: S): S {
+        redis[entity.accountId] = entity
+        return entity
+    }
+
+    override fun deleteById(id: String) {
+        redis.remove(id)
+    }
+
+    override fun findById(id: String): Optional<RefreshToken> =
+        Optional.ofNullable(redis[id])
+
+    override fun <S : RefreshToken?> saveAll(entities: MutableIterable<S>) = listOf<S>()
+    override fun existsById(id: String) = true
+    override fun findAll() = listOf<RefreshToken>()
+    override fun findAllById(ids: MutableIterable<String>) = listOf<RefreshToken>()
+    override fun count() = 0L
+    override fun delete(entity: RefreshToken) {}
+    override fun deleteAll(entities: MutableIterable<RefreshToken>) {}
+    override fun deleteAll() {}
 }
