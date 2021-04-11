55fa789de50fd80e93a5f47986ecef7a5afee936
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 22:15:35 +0900

    add request dto that logout.

--- src/main/kotlin/com/dsm/clematis/domain/authentication/controller/request/LogoutRequest.kt
+++ src/main/kotlin/com/dsm/clematis/domain/authentication/controller/request/LogoutRequest.kt
@@ -1,4 +1,9 @@
 package com.dsm.clematis.domain.authentication.controller.request
 
-class LogoutRequest {
-}
+import javax.validation.constraints.NotBlank
+
+data class LogoutRequest(
+
+    @get:NotBlank(message = "<NULL> <EMPTY> <BLANK>")
+    val token: String,
+)
