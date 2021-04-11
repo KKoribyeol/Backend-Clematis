b724a001c605fe799ac40565e7a7ab7c5de27c14
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:55:01 +0900

    add configuration that redis for use redis.

--- src/main/kotlin/com/dsm/clematis/global/configuration/RedisConfiguration.kt
+++ src/main/kotlin/com/dsm/clematis/global/configuration/RedisConfiguration.kt
@@ -1,12 +1,14 @@
 package com.dsm.clematis.global.configuration
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.CachingConfigurerSupport
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
-
 
 @Configuration
 @EnableRedisRepositories
@@ -21,7 +23,7 @@ class RedisConfiguration(
     fun redisConnectionFactory() = LettuceConnectionFactory(redisHost, redisPort)
 
     @Bean
-    fun redisTemplate(): RedisTemplate<*, *>? {
+    fun redisTemplate(): RedisTemplate<*, *> {
         val redisTemplate = RedisTemplate<ByteArray, ByteArray>()
         redisTemplate.setConnectionFactory(redisConnectionFactory())
         return redisTemplate
