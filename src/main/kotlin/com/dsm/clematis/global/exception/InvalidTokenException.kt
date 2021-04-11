656019c0963435f017e61d8767cc1bffd1851308
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:54:35 +0900

    add exception that invalid token.

--- src/main/kotlin/com/dsm/clematis/global/exception/InvalidTokenException.kt
+++ src/main/kotlin/com/dsm/clematis/global/exception/InvalidTokenException.kt
@@ -1,4 +1,9 @@
 package com.dsm.clematis.global.exception
 
-class InvalidTokenException {
-}
+import org.springframework.http.HttpStatus
+
+class InvalidTokenException : CommonException(
+    code = "INVALID_TOKEN",
+    message = "유효하지 않은 토큰입니다.",
+    status = HttpStatus.UNAUTHORIZED,
+)
