56971b83af96d5d4ed7ebd28911fb321dfae94af
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:55:16 +0900

    modify token.

--- src/main/kotlin/com/dsm/clematis/global/attribute/Token.kt
+++ src/main/kotlin/com/dsm/clematis/global/attribute/Token.kt
@@ -1,6 +1,12 @@
 package com.dsm.clematis.global.attribute
 
-enum class Token(val millisecondOfExpirationTime: Long) {
-    ACCESS(1000 * 60 * 60 * 4),
-    REFRESH(1000 * 60 * 60 * 24 * 7 * 2),
+enum class Token(val millisecondOfExpirationTime: Long, val kind: String) {
+    ACCESS(
+        millisecondOfExpirationTime = 1000 * 60 * 60 * 4,
+        kind = "accessToken"
+    ),
+    REFRESH(
+        millisecondOfExpirationTime = 1000 * 60 * 60 * 24 * 7 * 2,
+        kind = "refreshToken"
+    ),
 }
