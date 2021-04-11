3a3f4804acc412cf5ffd6df77c3fb1c6143911ce
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 22:17:50 +0900

    modify account service that create accounts.

--- src/main/kotlin/com/dsm/clematis/domain/account/service/AccountCreationService.kt
+++ src/main/kotlin/com/dsm/clematis/domain/account/service/AccountCreationService.kt
@@ -1,20 +1,15 @@
 package com.dsm.clematis.domain.account.service
 
 import com.dsm.clematis.domain.account.domain.Account
-import com.dsm.clematis.domain.account.exception.AccountNotFoundException
 import com.dsm.clematis.domain.account.exception.AlreadyExistAccountException
-import com.dsm.clematis.global.exception.PasswordMismatchException
 import com.dsm.clematis.domain.account.repository.AccountRepository
-import com.dsm.clematis.global.attribute.Token
 import com.dsm.clematis.global.security.provider.TokenProvider
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class AccountCreationService(
     private val accountRepository: AccountRepository,
-    private val tokenProvider: TokenProvider,
     private val passwordEncoder: PasswordEncoder,
 ) {
 
@@ -35,29 +30,5 @@ class AccountCreationService(
                 password = passwordEncoder.encode(accountPassword),
                 name = accountName,
             )
-        )
-
-    fun validateAccount(accountId: String, accountPassword: String): Boolean {
-        val findAccount = findAccountById(accountId)
-
-        return if (passwordEncoder.matches(accountPassword, findAccount.password))
-            true
-        else
-            throw PasswordMismatchException(accountPassword)
-    }
-
-    private fun findAccountById(accountId: String) =
-        accountRepository.findByIdOrNull(accountId) ?: throw AccountNotFoundException(accountId)
-
-    fun createAccessToken(accountId: String) =
-        tokenProvider.createToken(
-            accountId = accountId,
-            tokenType = Token.ACCESS,
-        )
-
-    fun createRefreshToken(accountId: String) =
-        tokenProvider.createToken(
-            accountId = accountId,
-            tokenType = Token.REFRESH,
         )
 }
