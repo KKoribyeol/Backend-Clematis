ee60b40c009e2000b7a788707d3cacb8011c2bce
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-11 21:47:23 +0900

    add test code that authentication create.

--- src/test/kotlin/com/dsm/clematis/domain/authentication/service/AuthenticationCreationServiceTest.kt
+++ src/test/kotlin/com/dsm/clematis/domain/authentication/service/AuthenticationCreationServiceTest.kt
@@ -1,6 +1,7 @@
 package com.dsm.clematis.domain.authentication.service
 
 import com.dsm.clematis.domain.account.domain.Account
+import com.dsm.clematis.domain.authentication.domain.RefreshToken
 import com.dsm.clematis.domain.authentication.repository.RedisRepository
 import com.dsm.clematis.global.attribute.Token
 import com.dsm.clematis.global.security.provider.TokenProvider
@@ -10,10 +11,10 @@ import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class AuthenticationServiceTest {
+internal class AuthenticationCreationServiceTest {
     private val tokenProvider = mockk<TokenProvider>()
     private val redisRepository = mockk<RedisRepository>()
-    private val testService = AuthenticationService(
+    private val testService = AuthenticationCreationService(
         tokenProvider = tokenProvider,
         redisRepository = redisRepository,
     )
@@ -26,6 +27,11 @@ internal class AuthenticationServiceTest {
 
     private val accessToken = "this-is-access-token"
     private val refreshToken = "this-is-refresh-token"
+
+    private val redisData = RefreshToken(
+        accountId = savedAccount.id,
+        refreshToken = refreshToken,
+    )
 
     @Test
     fun `엑세스 토큰 생성하기`() {
@@ -41,11 +47,13 @@ internal class AuthenticationServiceTest {
     @Test
     fun `리프레시 토큰 생성하기`() {
         every { tokenProvider.createToken(savedAccount.id, Token.REFRESH) } returns refreshToken
+        every { redisRepository.save(any()) } returns redisData
 
         val refreshToken = testService.createRefreshToken(savedAccount.id)
 
         assertThat(refreshToken).isEqualTo(this.refreshToken)
 
         verify(exactly = 1) { tokenProvider.createToken(savedAccount.id, Token.REFRESH) }
+        verify(exactly = 1) { redisRepository.save(any()) }
     }
 }
