7e0ef098599ff80e9c81cee8a9a126c19f4bebe3
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:08:53 +0900

    add test provider that push notification creation service.

--- src/test/kotlin/com/dsm/clematis/domain/push/service/TestPushNotificationCreationService.kt
+++ src/test/kotlin/com/dsm/clematis/domain/push/service/TestPushNotificationCreationService.kt
@@ -1,4 +1,38 @@
 package com.dsm.clematis.domain.push.service
 
-class TestPushNotificationCreationService {
+import com.dsm.clematis.domain.push.external.FirebasePushNotificationMessage
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Primary
+@Service
+class TestPushNotificationCreationService : PushNotificationCreationService {
+
+    override suspend fun sendAllPushNotification(
+        targetTokens: List<String>,
+        title: String,
+        body: String,
+        projectCode: String,
+    ) {}
+
+    override fun sendAllPushNotification(
+        targetTokens: List<String>,
+        title: String,
+        body: String,
+        reservationDate: LocalDateTime,
+        projectCode: String,
+    ) {}
+
+    override fun sendPushNotification(
+        targetToken: String,
+        title: String,
+        body: String,
+    ) = FirebasePushNotificationMessage.FirebaseMessage(
+        token = targetToken,
+        notification = FirebasePushNotificationMessage.FirebaseNotification(
+            title = title,
+            body = body,
+        ),
+    )
 }
