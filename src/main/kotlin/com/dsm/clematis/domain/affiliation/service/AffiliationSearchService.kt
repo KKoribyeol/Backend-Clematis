c3738be3d1c5a4bb1bf409fb2e07efeaca3a3abc
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 23:12:04 +0900

    add service that affiliation search.

--- src/main/kotlin/com/dsm/clematis/domain/affiliation/service/AffiliationSearchService.kt
+++ src/main/kotlin/com/dsm/clematis/domain/affiliation/service/AffiliationSearchService.kt
@@ -1,4 +1,18 @@
 package com.dsm.clematis.domain.affiliation.service
 
-class AffiliationSearchService {
+import com.dsm.clematis.domain.affiliation.repository.TargetAffiliationRepository
+import org.springframework.stereotype.Service
+
+@Service
+class AffiliationSearchService(
+    private val affiliationRepository: TargetAffiliationRepository,
+) {
+
+    fun getAffiliatedTarget(projectCode: String, groupNames: List<String>, targetTokens: List<String>) =
+        affiliationRepository.findByGroupProjectCodeAndGroupGroupNameIn(
+            projectCode = projectCode,
+            groupNames = groupNames,
+        ).map {
+            it.target.token
+        }.plus(targetTokens).distinct()
 }
