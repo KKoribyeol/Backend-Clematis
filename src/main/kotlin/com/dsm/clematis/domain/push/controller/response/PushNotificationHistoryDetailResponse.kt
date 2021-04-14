aa3d919a0e8eb0590d2f875544bdacc371795a80
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:54:47 +0900

    add response that push notification history detail.

--- src/main/kotlin/com/dsm/clematis/domain/push/controller/response/PushNotificationHistoryDetailResponse.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/controller/response/PushNotificationHistoryDetailResponse.kt
@@ -1,4 +1,14 @@
 package com.dsm.clematis.domain.push.controller.response
 
-class PushNotificationHistoryDetailResponse {
+data class PushNotificationHistoryDetailResponse(
+    val pageNumber: Int,
+    val totalPageNumber: Int,
+    val historyDetails: List<HistoryDetail>,
+    val isLastPage: Boolean,
+) {
+
+    data class HistoryDetail(
+        val targetToken: String,
+        val status: String,
+    )
 }
