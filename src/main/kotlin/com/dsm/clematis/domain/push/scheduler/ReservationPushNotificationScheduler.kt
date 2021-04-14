663e145f0bffabb8d00b9cf5604d3c48df63dfbf
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:42:16 +0900

    add scheduler that reservation push notification.

--- src/main/kotlin/com/dsm/clematis/domain/push/scheduler/ReservationPushNotificationScheduler.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/scheduler/ReservationPushNotificationScheduler.kt
@@ -1,4 +1,30 @@
 package com.dsm.clematis.domain.push.scheduler
 
-class PushNotificationHistoryScheduler {
+import com.dsm.clematis.domain.push.service.PushNotificationCreationService
+import com.dsm.clematis.domain.push.service.PushNotificationSearchService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Transactional
+class ReservationPushNotificationScheduler(
+    private val pushNotificationCreationService: PushNotificationCreationService,
+    private val pushNotificationSearchService: PushNotificationSearchService,
+) {
+
+    @Scheduled(cron = "0 1 * * * *")
+    suspend fun sendReservationPushNotification() {
+        val reservedPushNotification =
+            pushNotificationSearchService.searchReservedPushNotification()
+
+        reservedPushNotification.forEach {
+            pushNotificationCreationService.sendAllPushNotification(
+                targetTokens = it.pushResults.map { result -> result.target.token },
+                title = it.title,
+                body = it.body,
+                projectCode = it.project.code,
+            )
+        }
+    }
 }
