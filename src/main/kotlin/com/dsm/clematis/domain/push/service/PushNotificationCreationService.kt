2415da6a69236fb369b505df3968c73b8a0b78b0
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:41:54 +0900

    add service that push notification create.

--- src/main/kotlin/com/dsm/clematis/domain/push/service/PushNotificationCreationService.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/service/PushNotificationCreationService.kt
@@ -1,4 +1,25 @@
 package com.dsm.clematis.domain.push.service
 
+import com.dsm.clematis.domain.push.external.FirebasePushNotificationMessage.FirebaseMessage
+import java.time.LocalDateTime
+
 interface PushNotificationCreationService {
+    suspend fun sendAllPushNotification(
+        targetTokens: List<String>,
+        title: String,
+        body: String,
+        projectCode: String,
+    )
+    fun sendAllPushNotification(
+        targetTokens: List<String>,
+        title: String,
+        body: String,
+        reservationDate: LocalDateTime,
+        projectCode: String,
+    )
+    fun sendPushNotification(
+        targetToken: String,
+        title: String,
+        body: String,
+    ): FirebaseMessage?
 }
