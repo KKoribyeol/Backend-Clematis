a688c88bef104fbc966778dfbe800b2fc68bf16c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:39:27 +0900

    add firebase configuration.

--- src/main/kotlin/com/dsm/clematis/global/configuration/FirebaseConfiguration.kt
+++ src/main/kotlin/com/dsm/clematis/global/configuration/FirebaseConfiguration.kt
@@ -1,4 +1,22 @@
 package com.dsm.clematis.global.configuration
 
-class FirebaseConfiguration {
+import com.dsm.clematis.domain.push.external.FirebaseConnection
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+@Configuration
+class FirebaseConfiguration(
+    private val objectMapper: ObjectMapper,
+) {
+
+    @Bean
+    fun firebaseConnection() =
+        Retrofit.Builder()
+            .baseUrl("https://fcm.googleapis.com")
+            .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+            .build()
+            .create(FirebaseConnection::class.java)
 }
