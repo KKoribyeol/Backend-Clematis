b89442729ee82801dec6f5ac843b99755a7cfac8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 23:10:22 +0900

    add template push request.

--- src/main/kotlin/com/dsm/clematis/domain/push/controller/request/TemplatePushRequest.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/controller/request/TemplatePushRequest.kt
@@ -1,4 +1,26 @@
 package com.dsm.clematis.domain.push.controller.request
 
-class TemplatePushRequest {
-}
+import com.dsm.clematis.global.validation.NotDuplicate
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class TemplatePushRequest(
+
+    @get:NotEmpty(message = "<NULL> <EMPTY>")
+    @NotDuplicate
+    val targetTokens: List<String>,
+
+    @get:NotEmpty(message = "<NULL> <EMPTY>")
+    @NotDuplicate
+    val targetGroups: List<String>,
+
+    @get:NotNull(message = "<NULL>")
+    val templateId: Long,
+
+    @get:DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @get:JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    val reservationDate: LocalDateTime?
+)
