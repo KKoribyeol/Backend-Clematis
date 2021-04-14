8df34649fd3a4731cab21a6bca0ddbc42b7c7daf
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:47:19 +0900

    add exception that firebase push notification.

--- src/main/kotlin/com/dsm/clematis/domain/push/exception/FirebasePushNotificationException.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/exception/FirebasePushNotificationException.kt
@@ -1,4 +1,10 @@
 package com.dsm.clematis.domain.push.exception
 
-class FirebasePushNotificationException {
-}
+import com.dsm.clematis.global.exception.CommonException
+import org.springframework.http.HttpStatus
+
+class FirebasePushNotificationException : CommonException(
+    code = "FIREBASE_PUSH_NOTIFICATION",
+    message = "파이어베이스와 통신 중 문제가 발생하였습니다.",
+    status = HttpStatus.INTERNAL_SERVER_ERROR,
+)
