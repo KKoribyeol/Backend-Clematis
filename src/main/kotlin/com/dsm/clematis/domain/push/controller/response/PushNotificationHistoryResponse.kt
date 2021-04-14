91c6117610455958fccf2fa5e353737abe919505
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-14 22:54:09 +0900

    add response that push notiication history.

--- src/main/kotlin/com/dsm/clematis/domain/push/controller/response/PushNotificationHistoryResponse.kt
+++ src/main/kotlin/com/dsm/clematis/domain/push/controller/response/PushNotificationHistoryResponse.kt
@@ -1,4 +1,20 @@
 package com.dsm.clematis.domain.push.controller.response
 
-class PushNotificationHistoryResponse {
+import java.time.LocalDateTime
+
+data class PushNotificationHistoryResponse(
+    val pageNumber: Int,
+    val totalPageNumber: Int,
+    val histories: List<History>,
+    val isLastPage: Boolean,
+) {
+
+    data class History(
+        val id: Long,
+        val title: String,
+        val body: String,
+        val createdAt: LocalDateTime,
+        val reservedAt: LocalDateTime?,
+        val completedAt: LocalDateTime?,
+    )
 }
